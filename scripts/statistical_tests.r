d6943c36ee1e2544dde67f9a5284069e3909f56d
yelmokht <younes.el.mokhtari@ulb.be>
2024-04-27 17:18:41 +0200

    Correction version of implementation exercise 1

--- scripts/statistical_tests.r
+++ scripts/statistical_tests.r
@@ -3,20 +3,45 @@
 statistical_tests <- function(folder_path, output_path) {
     files <- list.files(folder_path, pattern="*.txt")
     solutions <- array(0, c(length(files), 78))
+
     for (i in 1:length(files)) {
         file_path <- paste(folder_path, files[i], sep="")
-        solutions[i,] <- (read.table(file_path)$V4) # Round to 5 decimal places
+        solutions[i,] <- (read.table(file_path)$V4)
     }
     student_table <- array(0, c(length(files), length(files)))
     wilcoxon_table <- array(0, c(length(files), length(files)))
+
     for (i in 1:(length(files) - 1)) {
         for (j in (i+1):length(files)) {
-            student_table[i, j] <- as.numeric(format(t.test(solutions[i,]+0.0001, solutions[j,]+0.001, paired=T)$p.value, scientific = TRUE, digits = 3))
-            wilcoxon_table[i, j] <- as.numeric(format(wilcox.test(solutions[i,]+0.001, solutions[j,]+0.0001, paired=T)$p.value, scientific = TRUE, digits = 3))
+            student_table[i, j] <- as.numeric(format(t.test(solutions[i,], solutions[j,], paired=T)$p.value, scientific = TRUE, digits = 3))
+            wilcoxon_table[i, j] <- as.numeric(format(wilcox.test(solutions[i,], solutions[j,], paired=T)$p.value, scientific = TRUE, digits = 3))
         }
     }
+
     write.table(student_table, paste(output_path, "student_t_test.txt"))
     write.table(wilcoxon_table, paste(output_path, "wilcoxon_test.txt"))
+
+    file_conn <- file(paste(output_path, "student_t_test.txt"), open = "a")
+
+    cat("\nStudent t-test:\n", file = file_conn)
+
+    for (i in seq_along(files)) {
+        cat(i, ": ", files[i], "\n", file = file_conn)
+    }
+
+    close(file_conn)
+
+        file_conn <- file(paste(output_path, "wilcoxon_test.txt"), open = "a")
+
+    cat("\nWilcoxon test\n", file = file_conn)
+
+    for (i in seq_along(files)) {
+        cat(i, ": ", files[i], "\n", file = file_conn)
+    }
+
+    close(file_conn)
+
+
 }
 
 statistical_tests("./statistics/experiments/", "./statistics/statistical_tests/")
--- src/sls/rules/pivoting.cpp
+++ src/sls/rules/pivoting.cpp
@@ -21,7 +21,7 @@ Solution& first_improvement(Neighbourhood neighbourhood_rule, Instance& instance
                 int new_score = solution.score() + delta(neighbourhood_rule, instance, solution, i, j);
 
                 if (new_score > solution.score()) {
-                    std::cout << "i: " << i << " j: " << j << " new_score: " << new_score << std::endl;
+                    // std::cout << "i: " << i << " j: " << j << " new_score: " << new_score << std::endl;
                     solution = apply_permutation(neighbourhood_rule, solution, i, j);
                     solution.set_score(new_score);
                     solution.set_last_neighbour({i, j});
